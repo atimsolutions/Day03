aceeefe5c2b06990a310a6c86ff69ace1846285a
Adnan Milisic <adnan@atimsolutions.se>
2021-03-10 10:14:10 +0100

    ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    //Only change code below this line
+
+    //Only change code below this line
+
+}
+console.log main(); //This is singel-line comment!
+/*module.exports = main;("This is multi-line comment!");
+*/
